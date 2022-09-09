9331b461c7dc9f89143892058686a8e6c65c87c2
AbelEwnetu <ebeloscom@gmail.com>
2022-09-09 14:09:46 -0700

    Smile

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - lowercase alphabet in reverse
+ * main - Lowercase alphabet in reverse
  *
  * Return: Always (Success)
  */
@@ -9,7 +9,7 @@ int main(void)
 {
 	char c;
 
-	for (c = 'z'; c <= 'a'; c--)
+	for (c = 'z'; c >= 'a'; c--)
 	{
 		putchar(c);
 	}
