f08b3d276570e1435634b53fde54f2a982706456
AbelEwnetu <ebeloscom@gmail.com>
2022-09-14 14:57:45 -0700

    2. 0 > 972?

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,5 +4,8 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
+int largest number(int a, int b, int c);
+void print remaining dayss(int month, int day, int year);
+int convert day(int month, int day);
 
 #endif /* MAIN_H */
