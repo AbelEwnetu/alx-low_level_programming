f9dd1505b621e3fd6f2a5a9c57aa85e48429bb54
AbelEwnetu <ebeloscom@gmail.com>
2022-09-15 13:33:40 -0700

    0-isupper

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,12 +1,10 @@
-#ifndef MAIN_H_INCLUDED
-#define MAIN_H_INCLUDED
+#ifndef MAIN_H
+#define MAIN_H
 
-#include <stdio.h>
-
-int _purchar(char c);
-int _isupper(int c);
-int _isdigit (int c);
-int mul(int a, int b);
+int _purchar(char);
+int _isupper(int);
+int _isdigit (int);
+int mul(int, int);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
