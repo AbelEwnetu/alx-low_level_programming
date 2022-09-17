f6dc8c3464f527763874969e4361fe0b32284dd6
AbelEwnetu <ebeloscom@gmail.com>
2022-09-17 10:58:21 -0700

    12. Numbers have life. not just symbols on paper

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,24 +1,22 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * print_number - function that prints an integer.
- * @n: number to print
+ * print_number - prints numbers
+ * @n: number to be printed
+ * Return:void
  */
 
 void print_number(int n)
 {
-	unsigned int x;
+unsigned int x;
 
-	x = n;
-	if (n < 0)
-	{
-		putchar('-');
-		x = -x;
-	}
-	if (x > 9)
-	{
-		print_number(x / 10);
-	}
-	putchar((x % 10) + '0');
+x = n;
+if (n < 0)
+{
+_putchar(45);
+x = -n;
+}
+if (x / 10)
+print_number(x / 10);
+_putchar((x % 10) + '0');
 }
