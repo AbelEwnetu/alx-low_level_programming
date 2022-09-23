c01889345edf08d869f48f0cd57ecdf82200145a
AbelEwnetu <ebeloscom@gmail.com>
2022-09-23 08:51:01 -0700

    11. Do not hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -12,16 +12,16 @@
 int main(void)
 {
 	char c;
-	int a;
+	int x;
 
 	srand(time(0));
-	while (a <= 2645)
+	while (x <= 2645)
 	{
 		c = rand() % 128;
-		a = a + c;
+		x = x + c;
 		putchar(c);
 	}
-	putchar(2772 - a);
+	putchar(2772 - x);
 
 	return (0);
 }
