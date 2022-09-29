8f89053a1cc556274723a6104a04af5e3f943c13
AbelEwnetu <ebeloscom@gmail.com>
2022-09-29 04:28:07 -0700

    7. They say we only use a fraction of our brain

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+int find_strlen(char *s);
+int check_palindrome(char *s, int len, int index);
+int is_palindrome(char *s);
+
+/**
+ * find_strlen - a function that returns length of string
+ * @s: string
+ *
+ * Return: length of the s
+ */
+
+int find_strlen(char *s)
+{
+	int len = 0;
+
+	if (*(s + len))
+	{
+		len++;
+		len += find_strlen(s + len);
+	}
+
+	return (len);
+}
