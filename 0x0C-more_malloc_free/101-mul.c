5cfdfa6370fe1639fe6f11635fd9261d3f320af5
AbelEwnetu <ebeloscom@gmail.com>
2022-10-07 09:07:27 -0700

    5. We must accept finite disappointment, but never lose infinite hope

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ * main - Program that multiplies two positive numbers.
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
+ * Return: returns integer.
+ */
+
+int main(int argc, char *argv[])
+{
+	unsigned long int mul;
+	int a, b;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	for (a = 1; a < argc; a++)
+	{
+		for (b = 0; argv[a][b] != '\0'; b++)
+		{
+			if (argv[a][b] > 57 || argv[a][b] < 48)
+			{
+				printf("Error\n");
+				exit(98);
+			}
+		}
+	}
+	mul = atol(argv[1]) * atol(argv[2]);
+	printf("%lu\n", mul);
+return (0);
+}
