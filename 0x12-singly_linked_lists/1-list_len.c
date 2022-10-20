040f05c13b53b67c83ba81e66a7b510b7395d4e5
AbelEwnetu <ebeloscom@gmail.com>
2022-10-20 10:10:25 -0700

    1. List length

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,19 +3,18 @@
  * list_len - a function that returns the number of elements
  *	in a linked list_t list.
  * @h: single linked list.
- *
  * Return: returns number of elements in the list.
  */
 
 size_t list_len(const list_t *h)
 {
-    size_t num_of_nodes;
+	size_t num_of_nodes;
 
-    num_of_nodes = 0;
-    while (h != NULL)
-    {
-        h = h->next;
-        num_of_nodes++;
-    }
-    return (num_of_nodes);
+	num_of_nodes = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		num_of_nodes++;
+	}
+	return (num_of_nodes);
 }
